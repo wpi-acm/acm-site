670dd348567ce52b49a567ceee60b68e78208dfd
Cara Salter <cara@devcara.com>
2024-03-03 20:15:31 -0500

    Scaffold main page

--- acmsite/main/__init__.py
+++ acmsite/main/__init__.py
@@ -5,3 +5,11 @@ bp = Blueprint('main', __name__)
 @bp.route("/")
 def homepage():
     return render_template("index.html")
+
+@bp.route("/events")
+def events():
+    return render_template("events.html")
+
+@bp.route("/join")
+def join():
+    return render_template("join.html")
